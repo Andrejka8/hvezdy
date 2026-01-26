491c5ea66471831df178b3d5ca273778fe92be72
Kulda83 <pepik.nitro38@gmail.com>
2026-01-26 12:15:15 +0100

    Add /Model/Zvire.cs

---
+++ utulek_2/Model/Zvire.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utulek_2.Model
+{
+    internal class Zvire
+    {
+        public int ID { get; set; } // automaticky generované
+        public string Jméno { get; set; } = string.Empty;
+        public string Druh { get; set; } = string.Empty; // pes/kočka/jiné
+        public int Věk { get; set; } // roky
+        public string Pohlaví { get; set; } = string.Empty;
+        public DateTime DatumPříjmu { get; set; } = DateTime.Today;
+        public string ZdravStav { get; set; } = string.Empty;
+        public string Poznámka { get; set; } = string.Empty;
+
+        public bool Adoptováno { get; set; }
+        public DateTime? DatumAdopce { get; set; }
+        
+    }
+}
+
--- utulek_2/Services/Evidence.cs
+++ utulek_2/Services/Evidence.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Utulek.Model;
+using utulek_2.Model;
 
 namespace utulek_2
 {
